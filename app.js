7cd90407466aac2072555646bfb84b36314488ee
SuppenSeebi <Seb.Schwinn@googlemail.com>
2025-07-17 21:34:35 +0200

    work on navbar

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-<!-----JavaScript for Toggle Menu-->
-var navLinks =document.getElementById("navLinks");
-function showMenu(){
-    navLinks.style.right="0";
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   ~~ NavBar Coloring ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+const navBoxes = document.querySelectorAll('.nav-box');
+const sections = document.querySelectorAll('section');
+
+function onScroll() {
+    let currentSectionId = 'top'; // Default
+
+    const scrollPos = window.scrollY || window.pageYOffset;
+
+    sections.forEach(section => {
+        const offsetTop = section.offsetTop;
+        const offsetHeight = section.offsetHeight;
+
+        if (scrollPos >= offsetTop - window.innerHeight / 2 && scrollPos < offsetTop + offsetHeight - window.innerHeight / 2) {
+            currentSectionId = section.id;
+        }
+    });
+
+    navBoxes.forEach(box => {
+        box.classList.remove('active', 'top');
+        console.log(currentSectionId)
+
+        // If this is the current nav. activate the box
+        if (box.getAttribute('data-section') === currentSectionId) {
+            box.classList.add('active');
+        }
+        // If box is top and current is not top, then update top box
+        if((box.getAttribute('data-section') === 'top') && (currentSectionId !== 'top')) {
+            box.classList.add('top');
+        }
+    });
 }
-function hideMenu(){
-    navLinks.style.right="-200px";
-}
 
-<!-----JavaScript for Scroll Bar-->
-window.addEventListener('scroll', function() {
-    const scrollProgress = document.querySelector(".scroll-progress");
-    const scrollTop = window.scrollY || window.pageYOffset;
-    const docHeight = document.documentElement.scrollHeight;
-    const winHeight = window.innerHeight;
-    const scrollable = docHeight - winHeight;
-    const scrolled = scrollable > 0 ? (scrollTop / scrollable) * 100 : 0;
-    scrollProgress.style.width = scrolled + "%";
-});
-
-<!-----Hide the Back To Top Arrow-->
-const backToTop = document.querySelector(".back-to-top");
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        backToTop.style.display = "block";
-    } else {
-        backToTop.style.display = "none";
-    }
-});
+window.addEventListener('scroll', onScroll);
+window.addEventListener('DOMContentLoaded', onScroll);
